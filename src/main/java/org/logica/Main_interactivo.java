dadeaa3cbf41b6afa74dc08593c7eec9c1c7dc66
Felipe Sepulveda <sepulvedafelipe2018@gmail.com>
2026-06-06 19:07:51 -0400

    refactor: actualizar Main_interactivo para adaptarlo al nuevo Comprador

--- src/main/java/org/logica/Main_interactivo.java
+++ src/main/java/org/logica/Main_interactivo.java
@@ -8,6 +8,8 @@ public class Main_interactivo{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Expendedor exp= new Expendedor(5);
+
+        Comprador comp = new Comprador();
         boolean activo=true;
         while(activo){
             System.out.println("INGRESE EL NUMERO DEL PRODUCTO QUE QUIERE");
@@ -16,6 +18,7 @@ public class Main_interactivo{
             System.out.println("5.SUPER 8");
             System.out.println("0.SALIR");
             int producto=sc.nextInt();
+
             if (producto==0){
                 System.out.println("SALIENDO    ");
                 activo=false;
@@ -41,41 +44,55 @@ public class Main_interactivo{
             System.out.print("3.1000    "); System.out.println("4.1500");
             System.out.println("0.SALIR");
             int maquinamoneda=sc.nextInt();
-            Moneda moneda=null;
-            if(maquinamoneda==1){
-                moneda=new Moneda100();
-            }
-            else if(maquinamoneda==2){
-                moneda=new Moneda500();
-            }
-            else if(maquinamoneda==3){
-                moneda=new Moneda1000();
-            }
-            else if(maquinamoneda==4){
-                moneda=new Moneda1500();
-            }
-            else if(maquinamoneda==0){
+
+            int valorElegido=0;
+            if (maquinamoneda==1) {
+                valorElegido=100;
+            } else if (maquinamoneda==2) {
+                valorElegido=500;
+            } else if (maquinamoneda==3) {
+                valorElegido=1000;
+            } else if (maquinamoneda==4) {
+                valorElegido=1500;
+            } else if (maquinamoneda==0) {
                 System.out.println("SALIENDO");
                 activo=false;
                 continue;
+            } else {
+                System.out.println("MONEDA INVALIDA");
+                continue;
             }
-            else {System.out.println("MONEDA INVALIDA");
-                continue;}
-            try {
-                Comprador c = new Comprador(moneda, tipo, exp);
 
-                System.out.println("tomaste: " + c.queBebiste());
-                System.out.println("tu vuelto es: " + c.cuantoVuelto());
+            Moneda moneda = comp.seleccionarmoneda(valorElegido);
+
+            if (moneda == null) {
+                System.out.println("No te quedan monedas de " + valorElegido + " en tu monedero");
+                continue;
+            }
+
+            try {
+                exp.comprarProducto(moneda, tipo);
+
+                comp.recogerProducto(exp);
+                System.out.println("Tomaste: " + comp.queBebiste());
 
             } catch (PagoIncorrectoException e) {
                 System.out.println(e.getMessage());
-
             } catch (PagoInsuficienteException e) {
                 System.out.println(e.getMessage());
-
             } catch (NoHayProductoException e) {
                 System.out.println(e.getMessage());
-                System.out.println();
+            } finally {
+                int vueltoRecogido = 0;
+                Moneda monedaVuelto;
+                monedaVuelto = comp.recogerVuelto(exp);
+                while (monedaVuelto != null) {
+                    vueltoRecogido += monedaVuelto.getValor();
+                    monedaVuelto = comp.recogerVuelto(exp);
+                }
+                if (vueltoRecogido > 0) {
+                    System.out.println("Recogiste: " + vueltoRecogido);
+                }
             }
 
         }
